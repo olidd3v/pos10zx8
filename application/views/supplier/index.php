13e82000b24d57e1c90dd169121635175e04e2b4
farhanunictive <kholid.farhan@unictive>
2022-07-01 14:45:08 +0700

    update

--- application/views/supplier/index.php
+++ application/views/supplier/index.php
@@ -37,12 +37,6 @@
                       <input type="submit" value="Cari" class="form-control btn btn-primary">
                     </div>
                   </div>
-                  <div class="col-md-3">
-                    <div class="form-group">
-                      <label for="submit">&nbsp</label>
-                      <a href="<?php echo site_url('supplier/export_csv').get_uri();?>" class="form-control btn btn-default"><i class="fa fa-file-excel-o"></i> Export Excel</a>
-                    </div>
-                  </div>
                 </div>
               </form>
               <table id="example1" class="table table-bordered table-striped">
